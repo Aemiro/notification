9e45d108ea05de845a4d7a818bfcb851987601b8
Aemiro Mekete <aemiromekete12@gmail.com>
2022-06-10 11:13:40 +0300

    Firts Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const http = require('http');
+const wsServer = require('./wsServer');
+
+var cors = require('cors');
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.text())
+app.use(express.static(__dirname + '/public'));
+app.use(cors())
+
+app.get('/api', (req, res) =>
+{
+    res.json({ message: `Welcome to Perago push notification api.` })
+})
+
+const server = http.createServer(app);
+const wss = wsServer();
+server.listen(Number(process.env.PORT), () =>
+{
+    console.log(`Server running on port ${process.env.PORT}`)
+
+})
---
+++ wsServer.js
@@ -0,0 +1,99 @@
+const WebSocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config()
+const msgType = {
+    Register: "register",
+    Logout: "logout",
+    AddToGroup: "addToGroup",
+    Send: "send"
+}
+let connectedDevices = [];
+module.exports = () =>
+{
+    const wsServer = new WebSocketServer({ port: process.env.WEBSOCKET_PORT })
+    wsServer.on('connection', function connection(ws, request)
+    {
+        const sessionId = uuidv4();
+        Object.assign(ws, { sessionId: sessionId });
+        console.log(ws.sessionId)
+        ws.on('message', function message(data)
+        {
+            try
+            {
+                const payload = JSON.parse(data)
+                if (payload.type)
+                {
+                    const type = payload.type;
+                    switch (type)
+                    {
+                        case msgType.Register:
+                            const user = {
+                                sessionId: ws.sessionId,
+                                ...payload.user
+                            };
+                            // console.log("user", user);
+                            connectedDevices.push(user);
+                            console.log(connectedDevices);
+                            break;
+                        case msgType.Send:
+                            const toUsers = connectedDevices.filter(c => c.id === payload.to);
+                            const fromUser = connectedDevices.find(c => c.sessionId === ws.sessionId);
+                            const msg = {
+                                sender: fromUser,
+                                msg: payload.data
+                            }
+                            const clients = [...wsServer.clients];
+                            const receivers = [];
+                            toUsers.forEach(user =>
+                            {
+                                const receiver = clients.find(client =>
+                                {
+                                    return client.sessionId === user.sessionId;
+                                });
+                                if (receiver)
+                                {
+                                    receivers.push(receiver);
+                                }
+                            })
+
+                            receivers.forEach(receiver =>
+                            {
+                                sendMessage(receiver, msg);
+                            })
+                            break;
+                    }
+
+                }
+                // console.log('at server', payload);
+                // wsServer.clients.forEach(function each(client)
+                // {
+                //     if (client.readyState === WebSocket.OPEN)
+                //     {
+                //         sendMessage(client, payload);
+                //     }
+                // });
+            } catch (err)
+            {
+                console.log(err)
+            }
+        });
+        ws.on('close', function connection(reason)
+        {
+            console.log(ws.sessionId, reason);
+            connectedDevices = connectedDevices.filter(d =>
+            {
+                return d.sessionId != ws.sessionId;
+            })
+            console.log(connectedDevices);
+        });
+    });
+    return wsServer;
+};
+const sendMessage = async (client, payload) =>
+{
+    if (client.readyState === WebSocket.OPEN)
+    {
+        client.send(JSON.stringify(payload));
+    }
+}
